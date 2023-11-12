56f8dec1126392b7de24661b940e4cde8c2f1637
franticboDev <it.franticbo@gmail.com>
2023-11-12 22:00:26 +0200

    Updated profile page. Added profile section in admin page. Added functionality to add pfps from admin page.

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
@@ -11,3 +13,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/pages/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/pages/logout.html'), name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
